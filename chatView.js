5bc7a46f2a1bd43335ae01e715e853187b300ac2
Manish Kumar <manishkumar13899@gmail.com>
2020-10-22 19:45:31 +0530

    Chat subs

--- chatView.js
+++ chatView.js
@@ -1,9 +1,39 @@
-const handleChatView = (db) => (req, res) => {
+const handleChatAllView = (db) => (req, res) => {
   const { uname, owner } = req.params;
   if (!uname && !owner) return res.status(400).json("Fill in all details!");
   db.select("*")
     .from("Chat")
-    .where({owner})
+    .where({owner,subscribermsg:false})
+    .then((data) => {
+      if (data.length) {
+        return res.json(data);
+      } else {
+        return res.status(404).json("username not found");
+      }
+    })
+    .catch((err) => res.status(400).json(err));
+};
+const handleChatSubsView = (db) => (req, res) => {
+  const { uname, owner } = req.params;
+  if (!uname && !owner) return res.status(400).json("Fill in all details!");
+  db.select("*")
+    .from("Chat")
+    .where({owner,subscribermsg:true})
+    .then((data) => {
+      if (data.length) {
+        return res.json(data);
+      } else {
+        return res.status(404).json("username not found");
+      }
+    })
+    .catch((err) => res.status(400).json(err));
+};
+const handleChatPrivateView = (db) => (req, res) => {
+  const { uname, owner } = req.params;
+  if (!uname && !owner) return res.status(400).json("Fill in all details!");
+  db.select("*")
+    .from("Chat")
+    .where({owner,uname})
     .then((data) => {
       if (data.length) {
         return res.json(data);
@@ -14,4 +44,4 @@ const handleChatView = (db) => (req, res) => {
     .catch((err) => res.status(400).json(err));
 };
 
-module.exports = { handleChatView };
+module.exports = { handleChatAllView,handleChatSubsView ,handleChatPrivateView  };
